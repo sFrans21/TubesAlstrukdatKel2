5538fed32e1527ded5a0e84c33db1161864fccfe
sFrans21 <118550565+sFrans21@users.noreply.github.com>
2023-10-31 14:18:17 +0700

    feat help command

---
+++ Command/help.h
@@ -0,0 +1,12 @@
+#ifndef HELP_H
+#define HELP_H
+
+void help();
+
+/* menampilkan daftar command yang mungkin untuk dieksekusi
+dengan deskripsinya.
+Penjelasan dari deskripsi dibebaskan selama masih
+mendeskripsikan command sesuai dengan spek.
+*/
+
+#endif
