8eaf122f89585aa04ee8683b840c125e129badee
Khayla Belva Annandira <130099275+khaylabelva@users.noreply.github.com>
2023-10-31 14:22:04 +0700

    Update mesinkarakter.h

--- ADT/mesinkarakter.h
+++ ADT/mesinkarakter.h
@@ -1,14 +1,36 @@
-#ifndef MESINKARAKTER_H
-#define MESINKARAKTER_H
+/* File: mesinkarakter.h */
+/* Definisi Mesin Karakter */
+
+#ifndef __MESIN_KAR_H_
+#define __MESIN_KAR_H_
 
 #include "boolean.h"
-#include <stdio.h>
 
-extern char CC;
-extern boolean EndFile;
+#define MARK '.'
+/* State Mesin */
+extern char currentChar;
+extern boolean EOP;
 
-void STARTCHAR(char *filename);
-void ADVCHAR();
-char GetCHARCC();
+void START();
+/* Mesin siap dioperasikan. Pita disiapkan untuk dibaca.
+   Karakter pertama yang ada pada pita posisinya adalah pada jendela.
+   Pita baca diambil dari stdin.
+   I.S. : sembarang
+   F.S. : currentChar adalah karakter pertama pada pita
+          Jika currentChar != MARK maka EOP akan padam (false)
+          Jika currentChar = MARK maka EOP akan menyala (true) */
+
+void ADV();
+/* Pita dimajukan satu karakter.
+   I.S. : Karakter pada jendela = currentChar, currentChar != MARK
+   F.S. : currentChar adalah karakter berikutnya dari currentChar yang lama,
+          currentChar mungkin = MARK
+          Jika  currentChar = MARK maka EOP akan menyala (true) */
+
+char GetCC();
+/* Mengirimkan currentChar */
+
+boolean IsEOP();
+/* Mengirimkan true jika currentChar = MARK */
 
 #endif
