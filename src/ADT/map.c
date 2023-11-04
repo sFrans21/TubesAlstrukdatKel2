350ae4bd196b54868d96389fb5b466753358a2f9
sFrans21 <118550565+sFrans21@users.noreply.github.com>
2023-11-04 11:06:48 +0700

    feat map.c , map.h

--- src/ADT/map.c
+++ src/ADT/map.c
@@ -1,97 +1,119 @@
-#include <stdio.h>
 #include "map.h"
 
-/* ********* Prototype ********* */
-
 /* *** Konstruktor/Kreator *** */
-void CreateEmptyMap(Map *M){
+void CreateEmptyMap(Map *M)
 /* I.S. Sembarang */
-/* F.S. Membuat sebuah Map M kosong berkapasitas MaxEl */
-/* Ciri Map kosong : count bernilai Nil */
-    (*M).Count = Nil;
+/* F.S. Membuat sebuah Map M kosong berkapasitas MaxElMap */
+/* Ciri Map kosong : count bernilai 0 */
+{
+    M->Count = 0;
 }
 
 /* ********* Predikat Untuk test keadaan KOLEKSI ********* */
-boolean IsEmptyMap(Map M){
+boolean IsEmptyMap(Map M)
 /* Mengirim true jika Map M kosong*/
-/* Ciri Map kosong : count bernilai Nil */
-    return M.Count == Nil;
+/* Ciri Map kosong : count bernilai 0 */
+{
+    return M.Count == 0;
 }
 
-boolean IsFullMap(Map M){
+boolean IsFullMap(Map M)
 /* Mengirim true jika Map M penuh */
-/* Ciri Map penuh : count bernilai MaxEl */
-    return M.Count == MaxEl;
+/* Ciri Map penuh : count bernilai MaxElMap */
+{
+    return M.Count == MaxElMap;
 }
 
 /* ********** Operator Dasar Map ********* */
-valuetype ValueMap(Map M, keytype k){
+valuetype Value(Map M, keytype k)
 /* Mengembalikan nilai value dengan key k dari M */
-/* Jika tidak ada key k pada M, akan mengembalikan Undefined */
-    int i = 0;
-    valuetype value;
-    boolean cek = false;
-    while ((i < M.Count) && !cek){
-        if (M.Elements[i].Key == k){
-            value = M.Elements[i].Value;
-            cek = true;
-        } else {
-            value = Undefined;
+{
+    boolean found = false;
+    addr idx = 0, iterator;
+
+    while (!found && idx < M.Count)
+    {
+        if (IsWordEq(M.Elements[idx].Key, k))
+        {
+            found = true;
         }
-        i++;
+        else
+        {
+            idx++;
+        }
     }
-    return value;
+
+    return M.Elements[idx].Value;
 }
 
-void InsertMap(Map *M, keytype k, valuetype v){
+void Insert(Map *M, keytype k, valuetype v)
 /* Menambahkan Elmt sebagai elemen Map M. */
 /* I.S. M mungkin kosong, M tidak penuh
         M mungkin sudah beranggotakan v dengan key k */
 /* F.S. v menjadi anggota dari M dengan key k. Jika k sudah ada, operasi tidak dilakukan */
-    if (!IsFull(*M)){
-        if (!IsMember(*M, k)){
-            (*M).Count++;
-            (*M).Elements[(*M).Count-1].Key = k;
-            (*M).Elements[(*M).Count-1].Value = v;
-        }
+{
+    if (IsMemberMap(*M, k))
+    {
+        return;
     }
+
+    M->Elements[M->Count].Key = k;
+    M->Elements[M->Count].Value = v;
+    M->Count++;
 }
 
-void DeleteMap(Map *M, keytype k){
+void Delete(Map *M, keytype k)
 /* Menghapus Elmt dari Map M. */
 /* I.S. M tidak kosong
         element dengan key k mungkin anggota / bukan anggota dari M */
 /* F.S. element dengan key k bukan anggota dari M */
-    if(IsMember(*M, k)){
-        boolean found = false;
-        int i = 0;
-        while ((i < (*M).Count) && !found){
-            if((*M).Elements[i].Key == k){
-                found = true;
-            } 
-            else {
-                i++;
-            }
-        }
-        while (i < (*M).Count){
-            (*M).Elements[i].Key = (*M).Elements[i+1].Key;
-            (*M).Elements[i].Value = (*M).Elements[i+1].Value;
-            i++;
-        }
-        (*M).Count--;
+{
+    boolean found = false;
+    addr idx = 0, iterator;
+
+    if (!IsMemberMap(*M, k))
+    {
+        return;
     }
+
+    while (!found && (idx < M->Count))
+    {
+        if (IsWordEq(M->Elements[idx].Key, k))
+        {
+            found = true;
+        }
+        else
+        {
+            idx++;
+        }
+    }
+
+    for (iterator = (idx + 1); iterator < M->Count; iterator++)
+    {
+        M->Elements[iterator - 1].Key = M->Elements[iterator].Key;
+        M->Elements[iterator - 1].Value = M->Elements[iterator].Value;
+    }
+
+    M->Count--;
 }
 
-boolean IsMemberMap(Map M, keytype k){
+boolean IsMemberMap(Map M, keytype k)
 /* Mengembalikan true jika k adalah member dari M */
+{
     boolean found = false;
-    int i = 0;
-    while ((i < M.Count) && !found){
-        if (M.Elements[i].Key == k) {
+    addr idx = 0, iterator;
+
+    while (!found && idx < M.Count)
+    {
+        if (IsWordEq(M.Elements[idx].Key, k))
+        {
             found = true;
-        } else {
-            i++;
+        }
+        else
+        {
+            idx++;
         }
     }
+
     return found;
 }
