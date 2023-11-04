81d598e54eb2488161eba8cffdb19ce88ba66f4b
sFrans21 <118550565+sFrans21@users.noreply.github.com>
2023-11-04 11:05:32 +0700

    feat set.h, set.c, map.h

--- src/ADT/map.h
+++ src/ADT/map.h
@@ -13,16 +13,18 @@ Deklarasi stack yang dengan implementasi array eksplisit-statik rata kiri
 #define Undefined -999
 
 // typedef int bool;
-typedef int keytype;
+typedef Word keytype;
 typedef int valuetype;
 typedef int address;
 
-typedef struct {
+typedef struct
+{
 	keytype Key;
 	valuetype Value;
 } infotype;
 
-typedef struct {
+typedef struct
+{
 	infotype Elements[MaxEl];
 	address Count;
 } Map;
@@ -56,13 +58,13 @@ valuetype ValueMap(Map M, keytype k);
 void InsertMap(Map *M, keytype k, valuetype v);
 /* Menambahkan Elmt sebagai elemen Map M. */
 /* I.S. M mungkin kosong, M tidak penuh
-        M mungkin sudah beranggotakan v dengan key k */
+	  M mungkin sudah beranggotakan v dengan key k */
 /* F.S. v menjadi anggota dari M dengan key k. Jika k sudah ada, operasi tidak dilakukan */
 
 void DeleteMap(Map *M, keytype k);
 /* Menghapus Elmt dari Map M. */
 /* I.S. M tidak kosong
-        element dengan key k mungkin anggota / bukan anggota dari M */
+	  element dengan key k mungkin anggota / bukan anggota dari M */
 /* F.S. element dengan key k bukan anggota dari M */
 
 boolean IsMemberMap(Map M, keytype k);
