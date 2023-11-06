44b730c281873a6d813ba8e8431e50a79826cfc8
sFrans21 <118550565+sFrans21@users.noreply.github.com>
2023-11-06 21:16:13 +0700

    add new config file

--- src/list.c
+++ src/list.c
@@ -1,17 +1,96 @@
-#include <stdio.h>
-#include "list.h"
 
-void list()
+
+// void list()
+// {
+//     FILE *file = fopen("ncus.txt", "r");
+//     if (file == NULL)
+//     {
+//         printf("Gagal membuka file konfigurasi.\n");
+//         return;
+//     }
+//     else
+//     {
+//         int BarisBerbaris;
+//         fscanf(file, "%d", &BarisBerbaris);
+//     }
+// }
+
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_ALBUMS 50
+#define MAX_SONGS_PER_ALBUM 20
+
+// Struktur untuk lagu
+struct Song
 {
-    FILE *file = fopen("config.txt", "r");
-    if (file == NULL)
+    char title[100];
+};
+
+// Struktur untuk album
+struct Album
+{
+    char name[100];
+    struct Song songs[MAX_SONGS_PER_ALBUM];
+    int numSongs;
+};
+
+// Fungsi untuk menampilkan album dan lagu dari map
+void printAlbumsAndSongs(struct Album albums[], int numAlbums)
+{
+    for (int i = 0; i < numAlbums; i++)
     {
-        printf("Gagal membuka file konfigurasi.\n");
-        return;
+        printf("Album: %s\n", albums[i].name);
+        printf("Songs:\n");
+        for (int j = 0; j < albums[i].numSongs; j++)
+        {
+            printf("- %s\n", albums[i].songs[j].title);
+        }
+        printf("\n");
     }
-    else
+}
+
+int main()
+{
+    struct Album albumMap[MAX_ALBUMS];
+    int numAlbums = 0;
+
+    // Baca file konfigurasi dan isi data ke map
+    FILE *file = fopen("ncus.txt", "r");
+    if (file)
+
     {
-        int BarisBerbaris;
-        fscanf(file, "%d", &BarisBerbaris);
+        char line[100];
+        char currentAlbum[100] = "";
+        while (fgets(line, sizeof(line), file))
+        {
+            if (line[0] == '#')
+            {
+                // Ini adalah komentar, abaikan
+                continue;
+            }
+
+            char *albumName = strtok(line, ";");
+            char *songTitle = strtok(NULL, ";");
+
+            if (strcmp(albumName, currentAlbum) != 0)
+            {
+                // Ini album baru
+                strcpy(currentAlbum, albumName);
+                numAlbums++;
+                strcpy(albumMap[numAlbums - 1].name, albumName);
+                albumMap[numAlbums - 1].numSongs = 0;
+            }
+
+            strcpy(albumMap[numAlbums - 1].songs[albumMap[numAlbums - 1].numSongs].title, songTitle);
+            albumMap[numAlbums - 1].numSongs++;
+        }
+
+        fclose(file);
     }
+
+    // Tampilkan album dan lagu dari map
+    printAlbumsAndSongs(albumMap, numAlbums);
+
+    return 0;
 }
--- src/list.h
+++ src/list.h
@@ -1,1 +1,80 @@
-void list();
+// void list();
+
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_ALBUMS 50
+#define MAX_SONGS_PER_ALBUM 20
+
+// Struktur untuk lagu
+struct Song
+{
+      char title[100];
+};
+
+// Struktur untuk album
+struct Album
+{
+      char name[100];
+      struct Song songs[MAX_SONGS_PER_ALBUM];
+      int numSongs;
+};
+
+// Fungsi untuk menampilkan album dan lagu dari map
+void printAlbumsAndSongs(struct Album albums[], int numAlbums);
+// {
+//       for (int i = 0; i < numAlbums; i++)
+//       {
+//             printf("Album: %s\n", albums[i].name);
+//             printf("Songs:\n");
+//             for (int j = 0; j < albums[i].numSongs; j++)
+//             {
+//                   printf("- %s\n", albums[i].songs[j].title);
+//             }
+//             printf("\n");
+//       }
+// }
+
+int main()
+{
+      struct Album albumMap[MAX_ALBUMS];
+      int numAlbums = 0;
+
+      // Baca file konfigurasi dan isi data ke map
+      FILE *file = fopen("ncus.txt", "r");
+      if (file)
+      {
+            char line[100];
+            char currentAlbum[100] = "";
+            while (fgets(line, sizeof(line), file))
+            {
+                  if (line[0] == '#')
+                  {
+                        // Ini adalah komentar, abaikan
+                        continue;
+                  }
+
+                  char *albumName = strtok(line, ";");
+                  char *songTitle = strtok(NULL, ";");
+
+                  if (strcmp(albumName, currentAlbum) != 0)
+                  {
+                        // Ini album baru
+                        strcpy(currentAlbum, albumName);
+                        numAlbums++;
+                        strcpy(albumMap[numAlbums - 1].name, albumName);
+                        albumMap[numAlbums - 1].numSongs = 0;
+                  }
+
+                  strcpy(albumMap[numAlbums - 1].songs[albumMap[numAlbums - 1].numSongs].title, songTitle);
+                  albumMap[numAlbums - 1].numSongs++;
+            }
+
+            fclose(file);
+      }
+
+      // Tampilkan album dan lagu dari map
+      printAlbumsAndSongs(albumMap, numAlbums);
+
+      return 0;
+}
