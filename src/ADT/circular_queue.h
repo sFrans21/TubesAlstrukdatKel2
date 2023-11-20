60b9ce777c8870cc09e540acf97fc32a7d59d943
DAPP <130123609+muhammaddaffa175@users.noreply.github.com>
2023-11-20 11:22:41 +0700

    circuler_queue

---
+++ src/ADT/circular_queue.h
@@ -0,0 +1,64 @@
+/* MODUL CIRCULAR CQUEUE */
+/* Definisi ADT CQueue dengan representasi array secara eksplisit dan alokasi statik */
+
+#ifndef __circular_Cqueue_h__
+#define __circular_Cqueue_h__
+
+#include "boolean.h"
+#include "mesinkata.h"
+#include "struc.h"
+#define IDX_UNDEF -1
+#define IDX_MAX 99
+
+/* Definisi tipe elemen dan indeks pada CQueue */
+typedef int ElType;
+
+/* ********* AKSES (Selektor) ********* */
+/* Jika q adalah CQueue, maka akses elemen : */
+#define CIDX_HEAD(q) (q).idxHead
+#define CIDX_TAIL(q) (q).idxTail
+#define     CHEAD(q) (q).Isi[(q).idxHead]
+#define     CTAIL(q) (q).Isi[(q).idxTail]
+
+/* ********* Prototype ********* */
+boolean CIsEmpty (QueueLagu Q);
+/* Mengirim true jika Q kosong */
+/* yaitu ketika idxHead=IDX_UNDEF dan idxTail=IDX_UNDEF */
+boolean CIsFull (QueueLagu Q);
+/* Mengirim true jika tabel penampung elemen Q sudah penuh */
+/* yaitu ketika idxHead=0 dan idxTail=IDX_MAX atau idxHead=idxTail+1 ketika idxHead > idxTail */
+int CLength (QueueLagu Q);
+/* Mengirimkan banyaknya elemen Q, 0 jika kosong */
+
+/* *** Kreator *** */
+void CCreateQueue (QueueLagu * Q);
+/* I.S. sembarang */
+/* F.S. mengembalikan Q kosong dengan kondisi sbb: */
+/* - idxHead=IDX_UNDEF; */
+/* - idxTail=IDX_UNDEF. */
+
+/* *** Primitif Add/Delete *** */
+void Cenqueue(QueueLagu *Q, char *penyanyi,  char *album,  char *lagu) ;
+/* Proses: Menambahkan X pada Q dengan aturan FIFO */
+/* I.S. Q mungkin kosong, tabel penampung elemen Q TIDAK penuh */
+/* F.S. Tail "maju" dengan mekanisme circular buffer,
+        X menjadi idxTail yang baru
+        Jika Q kosong, idxHead dimulai dari 0 */
+void Cdequeue(QueueLagu *Q, char *penyanyi, char *album, char *lagu) ;
+/* Proses: Menghapus idxHead pada Q dengan aturan FIFO, lalu mengembalikan nilainya */
+/* I.S. Q tidak mungkin kosong */
+/* F.S. mengembalikan nilai Q pada idxHead;
+        Head "maju" dengan mekanisme circular buffer;
+        Q mungkin kosong */
+
+/* *** Display CQueue *** */
+void CdisplayQueue(QueueLagu q);
+/* Proses : Menuliskan isi CQueue dengan traversal; CQueue ditulis di antara kurung 
+   siku; antara dua elemen dipisahkan dengan separator "koma" */
+/* I.S. q boleh kosong */
+/* F.S. Jika q tidak kosong: [e1,e2,...,en] */
+/* Contoh : jika ada tiga elemen bernilai 1, 20, 30 akan dicetak: [1,20,30] */
+/* Jika CQueue kosong : menulis [] */
+/* Note: Output mengandung newline */
+
+#endif
---
+++ src/ADT/struc.h
@@ -0,0 +1,123 @@
+#ifndef structure_H
+#define structure_H
+#include "boolean.h"
+#include <stdio.h>
+#include "ADT_Mesin-Kata/mesinkata.h" 
+
+typedef Word Title;
+typedef int Key;
+typedef int valuetype;
+
+#define MaxEl 100
+#define IDX_UNDEF -1
+#define INT_UNDEF -999
+#define Nil 0
+
+/*  Akses Elemen Lagu  */
+#define LAGU_NAMA(L, i, j, k, l) (L).penyanyi[i].album_penyanyi[j].album_lagu[k].nama_lagu[l]
+#define LAGU_ALBUM_ID(L, i, j, k) (L).penyanyi[i].album_penyanyi[j].album_lagu[k].album_id
+/*Melihat Daftar lagu yang dimiliki oleh penyanyi tertentu (LIST)*/
+// Berfungsi untuk memutar lagu (PLAY)
+
+/*  Akses Elemen Album  */
+#define ALBUM_NAMA(A, i, j, k) (A).penyanyi[i].album_penyanyi[j].nama_album[k]
+#define ALBUM_LAGU_LENGTH(A, i, j) (A).penyanyi[i].album_penyanyi[j].lagu_length
+#define ALBUM_PENYANYI_ID(A, i, j) (A).penyanyi[i].album_penyanyi[j].penyanyi_id
+/* Struktur Data Penyanyi yang memiliki Album (yang memiliki Lagu) [List Statis] */
+
+/* ### Akses Elemen Penyanyi ### */
+#define PENYANYI_NAMA(S, i, j) (S).penyanyi[i].nama_penyanyi[j]
+#define PENYANYI_ALBUM_LENGTH(S, i) (S).penyanyi[i].album_length
+
+/* Akses Elemen ListPenyanyi */
+#define LIST_PENYANYI_ID(LS, i) (LS).penyanyi[i]
+#define LIST_PENYANYI_LENGTH(LS) (LS).penyanyi_length
+
+/* Akses Elemen pada Playlist*/
+#define LIST_PLAYLIST_ID(LP, i) (LP).(*playlist + i)
+#define LIST_PLAYLIST_SIZE(LP) (LP).playlistSize
+// Berfungsi untuk Menambah Playlist  (PLAY)
+
+
+#define jumlah_lagu(l) (l).jumlah_lagu
+
+/* STRUKTUR DATA */
+/*  Indeks lagu [0..lagu_length] merupakan key dari map album ke lagu dengan
+    Nilai variabel nama_lagu merupakan value dari map Album -> Lagu */
+typedef struct {
+    Word nama_lagu;
+    int album_id;
+} Lagu;
+
+typedef struct {
+	Lagu A[MaxEl];  /* Memori tempat penyimpanan elemen (container) */
+	int jumlah_lagu;
+} SetLagu; 
+
+typedef struct {
+    Key album_id;
+    valuetype penyanyi_id;
+    Word nama_album;
+} Album;
+
+typedef struct {
+    Album Elements[MaxEl];
+    int count;
+} MapAlbum;
+
+typedef struct {
+    Title nama_penyanyi[MaxEl]; // Panjang nama penyanyi dibatasi 100 char
+    Album album_penyanyi[MaxEl/10]; // banyak album yang dimiliki artis dibatasi 10 album
+    int album_length;
+} Penyanyi;
+
+/* Struktur Data List Penyanyi (yang memiliki Album yang memiliki Lagu) [List Statis] */
+
+typedef struct {
+    Word lagu_playlist;
+    Title album_playlist[MaxEl];
+    Title Penyanyi_playlist[MaxEl];
+} isi_playlist;
+
+typedef struct {
+    Title playlist_nama[MaxEl];
+    isi_playlist playlist_user[MaxEl];
+    int isi;
+} NamaPlaylist; // Meninjau nama playlist (List Playlist)
+
+typedef struct {
+    NamaPlaylist* playlist;
+    size_t playlistSize;
+} ListPlaylist;
+
+typedef struct {
+    char *lagu_playlist;
+    char *album_playlist;
+    char *Penyanyi_playlist;
+} Isi_Que;
+
+typedef struct {
+    Isi_Que Isi[MaxEl+1];
+    int idxHead;
+    int idxTail;
+} QueueLagu;
+
+// Data Struct yang digunakan untuk antrian pada  queue lagu (Queue lagu & PLAY)
+
+typedef struct {
+    Isi_Que hist_lagu[MaxEl];
+    int idxTop;
+    int count;
+} HistoriLagu;
+
+
+typedef struct {
+    char *lagu;
+    char *album;
+    char *penyanyi;
+}status;
+
+extern status current;
+// Data Struct yang digunakan untuk menampung history lagu pada stack pada SONGS (Previous & Next)
+
+#endif
