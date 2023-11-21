d1d1f0933e98a360f9aa590ba7cbdfc31186d0bf
fislhhhhh <130198566+fislhhhhh@users.noreply.github.com>
2023-11-21 18:46:44 +0700

    Update status.h arraydin -> Dynamic

--- src/status.h
+++ src/status.h
@@ -2,7 +2,7 @@
 #define STATUS_H
 #include "start.h" // Termasuk file header yang diperlukan
 #include "ADT/stack.h"  // Mungkin memerlukan tipe data seperti Set untuk menyimpan lagu
-#include "ADT/arrayDin.h"   // Memerlukan Map untuk menyimpan album dan lagu
+#include "ADT/list_dinamis.h"  // Memerlukan Map untuk menyimpan album dan lagu
 #include "ADT/queue.h"  // Memerlukan List statis untuk daftar penyanyi
 
 #include "save.h"
@@ -15,6 +15,6 @@
 
 
 
-void displayStatus(StaticList *penyanyi, Map *penyanyiAlbums, maps *albumsong , Queue *UrutanLagu, ArrayDin *Playlist);
+void displayStatus(StaticList *penyanyi, Map *penyanyiAlbums, maps *albumsong , Queue *UrutanLagu, DynamicList *Playlist);
 
 #endif
