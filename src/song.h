db75269ca38c12e73bd9409d18923b84ca7dfc89
DAPP <130123609+muhammaddaffa175@users.noreply.github.com>
2023-11-21 10:41:59 +0700

    update song

--- src/song.h
+++ src/song.h
@@ -1,38 +1,18 @@
-#ifndef SONG_H
-#define SONG_H
 
-// Struktur data untuk representasi lagu
-typedef struct {
-    char singer[50];
-    char album[50];
-    char songtitle[50];
-} Song;
+#ifndef __SONG_H_
+#define __SONG_H_
 
-// Node untuk representasi elemen dalam queue
-typedef struct Node {
-    Song data;
-    struct Node* next;
-} Node;
+#include 
+#include "queue.h"  
 
-// Struktur data untuk representasi queue lagu
-typedef struct {
-    Node* head;
-    Node* tail;
-} SongQueue;
+void SongNext(QueueSong * queueS, RiwayatLagu * riwayat, CurrentSong * CurrentS)
+{
+    
+}
 
-// Fungsi untuk membuat node baru dalam queue
-Node* createNode(Song song);
+void SongPrev(QueueSong * queueS, RiwayatLagu * riwayat, CurrentSong * CurrentS)
+{
 
-// Fungsi untuk membuat queue lagu baru
-SongQueue* createQueue();
+}
 
-// Fungsi untuk menambahkan lagu ke dalam queue
-void enqueuesong(SongQueue* queue, Song song);
-
-// Fungsi untuk memutar lagu selanjutnya dalam queue
-void playNextSong(SongQueue* queue);
-
-// Fungsi untuk memutar lagu sebelumnya dalam queue
-void playPreviousSong(SongQueue* queue, Song currentSong);
-
-#endif // SONG_H
+#endif
