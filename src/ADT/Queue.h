df34219a3b8f4a29832d194ab5c0badbbdec03e9
khairunnize <118610372+khairunnize@users.noreply.github.com>
2023-11-23 17:01:56 +0700

    ubah adt

--- src/ADT/Queue.h
+++ src/ADT/Queue.h
@@ -8,22 +8,28 @@
 #define CAPACITY 100
 
 /* Definisi elemen dan address */
-typedef struct {
-	Word buffer[CAPACITY]; 
+typedef struct{
+	Word SingerName;
+	Word AlbumName;
+	Word SongTitle;
+} Song;
+
+typedef struct{
+	Song QueueDetail[CAPACITY];
 	int idxHead;
 	int idxTail;
-} Queue;
+} SQueue;
 
 
 /* ********* AKSES (Selektor) ********* */
 /* Jika q adalah Queue, maka akses elemen : */
 #define IDX_HEAD(q) (q).idxHead
 #define IDX_TAIL(q) (q).idxTail
-#define     HEAD(q) (q).buffer[(q).idxHead]
-#define     TAIL(q) (q).buffer[(q).idxTail]
+#define     HEAD(q) (q).QueueDetail[(q).idxHead]
+#define     TAIL(q) (q).QueueDetail[(q).idxTail]
 
 /* *** Kreator *** */
-void CreateQueue(Queue *q);
+void CreateQueue(SQueue *q);
 /* I.S. sembarang */
 /* F.S. Sebuah q kosong terbentuk dengan kondisi sbb: */
 /* - Index head bernilai IDX_UNDEF */
@@ -31,36 +37,36 @@ void CreateQueue(Queue *q);
 /* Proses : Melakukan alokasi, membuat sebuah q kosong */
 
 /* ********* Prototype ********* */
-boolean isQueueEmpty(Queue q);
+boolean isQueueEmpty(SQueue q);
 /* Mengirim true jika q kosong: lihat definisi di atas */
-boolean isQueueFull(Queue q);
+boolean isQueueFull(SQueue q);
 /* Mengirim true jika tabel penampung elemen q sudah penuh */
-/* yaitu IDX_TAIL akan selalu di belakang IDX_HEAD dalam buffer melingkar*/
+/* yaitu IDX_TAIL akan selalu di belakang IDX_HEAD dalam QueueDetail melingkar*/
 
-int lengthQueue(Queue q);
+int lengthQueue(SQueue q);
 /* Mengirimkan banyaknya elemen queue. Mengirimkan 0 jika q kosong. */
 
 /* *** Primitif Add/Delete *** */
-void enqueue(Queue *q, Word val);
+void enqueue(SQueue *q, Song val);
 /* Proses: Menambahkan val pada q dengan aturan FIFO */
 /* I.S. q mungkin kosong, tabel penampung elemen q TIDAK penuh */
-/* F.S. val menjadi TAIL yang baru, IDX_TAIL "mundur" dalam buffer melingkar. */
+/* F.S. val menjadi TAIL yang baru, IDX_TAIL "mundur" dalam QueueDetail melingkar. */
 
-void dequeue(Queue *q, Word *val);
+void dequeue(SQueue *q, Song *val);
 /* Proses: Menghapus val pada q dengan aturan FIFO */
 /* I.S. q tidak mungkin kosong */
 /* F.S. val = nilai elemen HEAD pd I.S., IDX_HEAD "mundur";
         q mungkin kosong */
 
 /* *** Display Queue *** */
-void displayQueue(Queue q);
-/* Proses : Menuliskan isi Queue dengan traversal, Queue ditulis di antara kurung 
-   siku; antara dua elemen dipisahkan dengan separator "koma", tanpa tambahan 
-   karakter di depan, di tengah, atau di belakang, termasuk spasi dan enter */
-/* I.S. q boleh kosong */
-/* F.S. Jika q tidak kosong: [e1,e2,...,en] */
-/* Contoh : jika ada tiga elemen bernilai 1, 20, 30 akan dicetak: [1,20,30] */
-/* Jika Queue kosong : menulis [] */
+// void displayQueue(SQueue q);
+// /* Proses : Menuliskan isi Queue dengan traversal, Queue ditulis di antara kurung 
+//    siku; antara dua elemen dipisahkan dengan separator "koma", tanpa tambahan 
+//    karakter di depan, di tengah, atau di belakang, termasuk spasi dan enter */
+// /* I.S. q boleh kosong */
+// /* F.S. Jika q tidak kosong: [e1,e2,...,en] */
+// /* Contoh : jika ada tiga elemen bernilai 1, 20, 30 akan dicetak: [1,20,30] */
+// /* Jika Queue kosong : menulis [] */
 
 
 #endif
